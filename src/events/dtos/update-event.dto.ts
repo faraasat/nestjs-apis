ba5da5523498a8d4a574edfb2242e3f79bf95298
Farasat Ali <farasat.ali@starmarketingonline.com>
2023-09-13 11:47:25 +0500

    Fox

--- src/events/dtos/update-event.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateEventDto } from './create-event.dto';
-
-// this partial type makes the all properties to optional
-export class UpdateEventDto extends PartialType(CreateEventDto) {}
